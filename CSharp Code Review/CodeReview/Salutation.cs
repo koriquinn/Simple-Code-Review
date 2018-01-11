3656ea53eb7d64066f767cf1a16ac8cc2df8d56a
KoriWalsh <korissarobertson@gmail.com>
2018-01-11 16:06:11 -0700

    More Objects

--- CSharp Code Review/CodeReview/Salutation.cs
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace CodeReview
 {
+    /// <summary>
+    /// The Salutation class represents something that can say hello and goodbye
+    /// </summary>
     public class Salutation
     {
+  
+        #region Properties and Fields - What the Salutatiion objects will "look like"
+        // Fields
         private string _Greeting;
         private string _Farewell;
+        #endregion
 
+        #region Constructor and Methods - How the Salutation objects will "behave"
+        /// <summary>
+        /// Instantiates a Salutation object with appropriate messages
+        /// </summary>
+        /// <param name="greetingMessage"> The message to use when greeting</param>
+        /// <param name="farewellMessage">The message to use to say goodbye</param>
         public Salutation(string greetingMessage, string farewellMessage)
         {
             _Greeting = greetingMessage;
@@ -26,5 +39,6 @@ namespace CodeReview
         {
             return _Farewell;
         }
+        #endregion
     }
 }
